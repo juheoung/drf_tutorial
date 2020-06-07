0436e0f5350aac3036aae152b5483b3def090df1
juheoung <juheoung0201@gmail.com>
2020-06-07 18:10:57 +0900

    drf_tutorial

---
+++ drf_tutorial/drf_tutorial/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.urls import path
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('snippets.urls')),
+]
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
+]
